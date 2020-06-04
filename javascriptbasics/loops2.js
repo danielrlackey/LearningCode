bd5af8ca23dda1e56c6166842794e9631073a8ba
danielrlackey <65509781+danielrlackey@users.noreply.github.com>
2020-06-04 19:33:54 -0400

    Add files via upload

---
+++ javascriptbasics/loops2.js
@@ -0,0 +1,31 @@
+// let num = -10;
+
+// while(num <= 19){
+//     console.log(num)
+//     num++
+// }
+
+// let num = 10;
+// while(num <= 40){
+
+//     if(num%2 == 0){
+//         console.log(num)
+//     }
+//     num++
+// }
+
+// let num = 300
+// while( num <= 333){
+//     if(num%3 == 0){
+//         console.log(num)
+//     }
+//     num++
+// }
+
+let num = 5
+while(num <= 50){
+    if(num % 5 == 0 && num % 3 == 0){
+        console.log(num)
+    }
+    num++
+}
