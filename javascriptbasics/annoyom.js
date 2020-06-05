1a31159fbffa27075d3f285c5b4099c6f5422d46
danielrlackey <65509781+danielrlackey@users.noreply.github.com>
2020-06-05 19:33:18 -0400

    Add files via upload

---
+++ javascriptbasics/annoyom.js
@@ -0,0 +1,10 @@
+let answer = prompt("Are we there yet?");
+
+if(answer === "yes"){
+    alert("Yay we made it!")
+}  
+
+while(answer !== "yes"){
+    let answer = prompt("are we there yet?");
+}
+
---
+++ javascriptbasics/forloops.js
@@ -0,0 +1,29 @@
+let str = "hello";
+
+for(let i = 0; i < str.length; i++){
+    console.log(str[i]);
+}
+
+
+
+for(let i = -10; i < 20; i++){
+    console.log(i)
+}
+
+for( let i = 10; i < 41; i++){
+    if(i % 2 === 0){
+        console.log(i)
+    }
+}
+
+for(let i = 300; i < 334; i++){
+    if(i % 3 === 0){
+        console.log(i)
+    }
+}
+
+for(let i = 5; i < 51; i++){
+    if(i % 3 === 0 && i % 5 === 0){
+        console.log(i)
+    }
+}
