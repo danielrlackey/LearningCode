b4f7fe1b8975f058e0194b8038b811d3af828474
danielrlackey <65509781+danielrlackey@users.noreply.github.com>
2020-06-09 21:10:33 -0400

    Add files via upload

--- javascriptbasics/objects.js
+++ javascriptbasics/objects.js
@@ -1,50 +1,38 @@
-const arr =[1, 2, 3, 4];
+// const someObject = {
+//     friends: [
+//         {name:"malfoy"},
+//         {name:"grabble"},
+//         {name:"goyle"}
+//     ],
+//     color: "baby blue",
+//     isEvil: true
+// };
 
-const rev = arr.reverse();
-
-console.log(rev);
-
-const letters =["a ", "b ", "c"];
-
-const rev2 = letters.reverse();
-
-for (i = 0; i < 4; i++){
-    console.log(letters[i])
-}
+// console.log(someObject.friends[0])
 
 
-function sum(arr){
-let s = 0;
-for(let i = 0; i < arr.length; i++){
-    s = s + arr[i]
-   }
-   console.log(s)
-}
+const movies = [
+   {
+       title: "the godfather",
+       rating: "5 stars",
+       watched: true
+   },
+   {
+       title: "man on fire",
+       rating: "5 stars",
+       watched: true
+   },
+   {
+       title: "frozen",
+       rating: "unknown",
+       watched: false
+   },
+]
 
-
-sum([73, 29, 45, 90, 34])
-
-add ([2, 10, 69, 40])
-
-function add(arra){
-    add = 0;
-    for(let i = 0; i < arra.length; i++){
-        add = add + arra[i]
+for(let i = 0; i < movies.length; i++){
+    if(movies[i].watched === true){
+    console.log("you have watched" + " " + movies[i].title + " "  + "and you rated the movie" + " " + movies[i].rating)
+    } else if (movies[i].watched === false){
+        console.log("you have not seen" + " " + movies[i].title + " " + "therefore you cannot rate it")
     }
-    console.log(add)
 }
-
-
-const numms =[1, 2, 3, 4, 5];
-
-function largest (numms){
-highest = 0;
-for(let i = 0; i < numms.length; i++){
-    if(numms[i] > highest){
-        highest = numms[i];
-        }
-    
-    }
-    return highest;
-}
-console.log(largest(numms))
