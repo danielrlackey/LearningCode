76eeb653f852b057865f2a52427d46eccae40ba3
danielrlackey <65509781+danielrlackey@users.noreply.github.com>
2020-06-11 21:39:48 -0400

    Add files via upload

---
+++ domManipulation/selector.js
@@ -0,0 +1,9 @@
+let tag = document.getElementById("highlight");
+
+console.log(tag)
+
+let tags = document.getElementsByClassName("bolded");
+console.log(tags[0])
+
+let tagz = document.getElementsByTagName("li");
+console.log(tagz);
