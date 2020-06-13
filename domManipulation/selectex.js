9a316959df543661706cd502fc1f60bfa7cbe784
danielrlackey <65509781+danielrlackey@users.noreply.github.com>
2020-06-12 22:55:00 -0400

    Add files via upload

---
+++ domManipulation/selectex.js
@@ -0,0 +1,16 @@
+let yolo = document.getElementsByClassName("special")[1]
+console.log(yolo)
+
+let h1 = document.querySelector("h1");
+
+h1.style.color = "yellow";
+h1.style.border = "5px solid pink";
+
+let p = document.querySelector('p')
+console.log(p)
+
+console.log(p.textContent);
+let ul = document.getElementsByTagName("ul")[0];
+console.log(ul);
+
+console.log(ul.textContent);
